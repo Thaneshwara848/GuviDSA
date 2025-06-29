07506eacc794e587ca7bb1fdcd8808709b6563d9
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2025-06-29 13:21:33 +0530

    sort and search

---
+++ Day4/src/comD/Demo1.java
@@ -0,0 +1,34 @@
+package comD;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Demo1 {
+	public static void main(String[] args) {
+		int arr[]= {11 ,22 , 33, 44, 55 };
+//	Search :  	Linear, Binary, Ternary
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ENter the Element to Search .....!");
+		int key = sc.nextInt();
+		boolean found = false ;
+
+		for(int num :arr) {
+			if(num == key) {
+				found = true;
+				break;
+			}
+		}
+		if(found) {
+			System.out.println("Yes Element Found....!");
+		}
+		else {
+			System.out.println("Element Not Found...!");
+		}
+		System.out.println("=====SORT  :BubbleInsertion /Selection /Quick /Merge ====");
+		
+		int ar[]= {33,22,11,44,55,88,66};
+		Arrays.sort(ar);
+		System.out.println("Sorted Array : ");
+		for(int num :ar) {
+			System.out.println(num);
+		}
+	}
+}
